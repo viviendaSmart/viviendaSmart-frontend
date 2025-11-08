d8a5d9e0b2fa7d1813ddfa1804a91690591dda0a
RafaelVivanco <rafaelvivanco2@gmail.com>
2025-11-08 13:19:40 -0500

    clients connected to db

---
+++ src/app/features/client/models/client.entity.ts
@@ -0,0 +1,32 @@
+﻿export class Client{
+  dni!: string;
+
+  monthlyIncome!: number;
+
+  address!: string;
+
+  maritalStatus!: string;
+
+  phoneNumber!: string;
+
+  userId!: string;
+
+  constructor(client:{
+    dni: string;
+    monthlyIncome: number;
+    address: string;
+    maritalStatus: string;
+    phoneNumber: string;
+    userId: string;
+  }
+  ) {
+    this.dni = client.dni;
+    this.monthlyIncome = client.monthlyIncome;
+    this.address = client.address;
+    this.maritalStatus = client.maritalStatus;
+    this.phoneNumber = client.phoneNumber;
+    this.userId = client.userId;
+  }
+
+
+}
