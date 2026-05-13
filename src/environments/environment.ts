5fb283731567e1695b20f7f6a255817c89a6eb0a
RafaelVivanco <rafaelvivanco2@gmail.com>
2026-05-13 18:10:07 -0500

    chore: update serverBaseUrl to remote frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
-﻿export const environment={
-  production:true,
-  serverBaseUrl:'http://localhost:8080/api/v1',
-  usersEndpointPath:'/users',
-  registerEndpointPath:'/users/signup',
-  loginEndpointPath:'/users/signin',
-  propertyEndpointPath:'/properties',
-  clientEndpointPath:'/clients',
-  configEndpointPath:'/config',
-  simulatorEndpointPath:'/simulator'
+﻿export const environment = {
+  production: true,
+  serverBaseUrl: 'https://vivendasmart-backend-production.up.railway.app/api/v1',
+  usersEndpointPath: '/users',
+  registerEndpointPath: '/users/signup',
+  loginEndpointPath: '/users/signin',
+  propertyEndpointPath: '/properties',
+  clientEndpointPath: '/clients',
+  configEndpointPath: '/config',
+  simulatorEndpointPath: '/simulator'
 };
