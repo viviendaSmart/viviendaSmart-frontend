4c18c07b98f83d0e846e0c8fd1c9c6ca1e9cd59c
RafaelVivanco <rafaelvivanco2@gmail.com>
2026-06-16 08:59:09 -0500

    feat: add export to xlsx button in simulations

--- src/app/features/simulator/components/response-form-modal/response-form-modal.ts
+++ src/app/features/simulator/components/response-form-modal/response-form-modal.ts
@@ -1,6 +1,7 @@
 import { TranslateModule } from '@ngx-translate/core';
 import {Component, Input, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ExcelExportService } from '../../../../shared/services/excel-export.service';
 
 @Component({
   selector: 'app-response-form-modal',
@@ -15,4 +16,11 @@ export class ResponseFormModal {
   // Podrías tiparlo como SimulationResult si ya tienes la interfaz en el frontend
   @Input() simulationResult: any | null = null;
 
+  constructor(private excelExportService: ExcelExportService) {}
+
+  exportToExcel() {
+    if (this.simulationResult) {
+      this.excelExportService.exportSimulation(this.simulationResult, 'Simulacion_Credito_Hipotecario');
+    }
+  }
 }
