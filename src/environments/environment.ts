4692a0fd9d638c275bc777b40cb4802bbb9d2175
RafaelVivanco <rafaelvivanco2@gmail.com>
2026-07-08 19:05:47 -0500

    chore: remote backend connected

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 ﻿export const environment = {
   production: true,
-  serverBaseUrl: 'viviendasmart-backend-byd0b3gud9heagdx.mexicocentral-01.azurewebsites.net/api/v1',
+  serverBaseUrl: 'https://viviendasmart-backend-byd0b3gud9heagdx.mexicocentral-01.azurewebsites.net/api/v1',
   usersEndpointPath: '/users',
   registerEndpointPath: '/users/signup',
   loginEndpointPath: '/users/signin',
